49fc838b3f4246db50ff2d73668b4612d903fbe6
René-M Kruse <rene-marcel.kruse@uni-goettingen.de>
2023-08-16 15:27:20 +0200

    Aufräumen.

--- klausur_bayesmodel.R
+++ klausur_bayesmodel.R
@@ -1,31 +1,13 @@
 library(readr)
-daten_aufgabe2 <- read_csv("~/Downloads/daten_aufgabe2.csv")
+daten_aufgabe2 <- read_csv("daten2.csv")
+
+
+# daten_aufgabe2$x2 <- daten_aufgabe2$x2/10
+# daten_aufgabe2$x2 <- daten_aufgabe2$x2/10
+# daten_aufgabe2$y <- daten_aufgabe2$y -150 
+# daten_aufgabe2$y <- daten_aufgabe2$y * 0.8
 
 hist(daten_aufgabe2$y)
 
-sd_werte = aggregate(data$Reaction, list(data$Subject), FUN=sd) 
-sd(sd_werte$x)
-
-data =  lme4::sleepstudy
-summary(data)
-
-# library(dplyr)
-# 
-# data %>%
-#   group_by(Subject) %>%
-#   summarise_at(vars(Reaction), list(name=sd))  %>%
-
-
-m3 = brm(Reaction ~ 1 + Days + (1 | Subject) ,
-         family = gaussian(),
-         data = data,
-         prior = c(
-           prior(normal(300, 100), class=Intercept),
-           prior(normal(0, 60), class = sigma),
-           prior(normal(4.5, 3), class = b, coef = Days),
-           prior(normal(0, 30), class = sd, coef=Intercept , group = Subject)
-           ),
-         cores = 10)
-summary(m3)
-
-summary(lme4::lmer(Reaction ~ 1 + Days + (1 | Subject), data = data))
+summary(lm(y ~ x1 + x2, data = daten_aufgabe2))
+write.csv(daten_aufgabe2, "daten2.csv", row.names=FALSE)
